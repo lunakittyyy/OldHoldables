4e9442c07c848865fff40f550107cb3336326caa
Lillie <lennon.m.craig@gmail.com>
2023-05-07 19:39:09 -0400

    Update PluginInfo.cs

--- OldHoldables/PluginInfo.cs
+++ OldHoldables/PluginInfo.cs
@@ -5,7 +5,7 @@
     /// </summary>
     internal class PluginInfo
     {
-        public const string GUID = "com.kaylie.gorillatag.oldholdables";
+        public const string GUID = "com.luna.gorillatag.oldholdables";
         public const string Name = "OldHoldables";
         public const string Version = "2.0.0";
     }
